170e9e2132b2c22b76e25837e729d2ae6b907fd5
Hristijan <hristijanpetlovski@gmail.com>
2020-11-15 19:57:33 +0100

    Change homework-5

---
+++ homeworks/LetandConst.js
@@ -0,0 +1,10 @@
+function main() {
+  // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+
+  const r =readLine();    
+  let area = (Math.PI * (r * r)); 
+  let perimetar = (2 * Math.PI * r);
+  // Print the area of the circle:
+ console.log(area);
+  // Print the perimeter of the circle:
+  console.log(perimetar);
